1d6f54c487b96297ec59c51d960b961938262900
Devon Weller <devon@tokenly.co>
2017-01-27 12:53:53 -0600

    Redirect to intended URL after initial registration

--- app/Http/Controllers/Accounts/DashboardController.php
+++ app/Http/Controllers/Accounts/DashboardController.php
@@ -3,6 +3,8 @@
 namespace Tokenpass\Http\Controllers\Accounts;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 use Tokenpass\Http\Controllers\Controller;
 
 class DashboardController extends Controller
@@ -19,6 +21,13 @@ class DashboardController extends Controller
 
 
     public function getDashboard() {
+        // if we just registered while on our way to granting an application access
+        //   go there now
+        $intended_url = Session::pull('url.intended', null);
+        if ($intended_url) {
+            return redirect($intended_url);
+        }
+
         return view('accounts/dashboard', [
             'user' => Auth::user(),
         ]);
